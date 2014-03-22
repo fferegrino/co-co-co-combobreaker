68b52cd64d48145a6a4a46bba08995d55f9256d0
Antonio Feregrino <antonio@fferegrino.org>
2014-03-21 21:34:27 -0600

    Train Swapping

--- coj/trainswapping.c
+++ coj/trainswapping.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
-
+int trains[51];
 int main(){
-	int t, l, i, n = 0;
+	int t, l, i, n = 0,c = 1;
 	scanf("%d",&t);
 	while(t--){
 		scanf("%d",&l);
-		int * trains = (int *)malloc(sizeof(int) * l);
 		for(i = 0; i < l; i++){
 			scanf("%d",&trains[i]);
 		}
-		
-		free(trains);
+		while(c){
+			c = 0;
+			for(i = 1; i < l; i++){
+				if(trains[i-1] > trains[i]){
+					int a =  trains[i-1];
+					trains[i-1] = trains[i];
+					trains[i] = a;
+					n++;
+					c = 1;
+				}
+			}
+			l--;
+		}
+		printf("Optimal train swapping takes %d swaps.\n",n);
+		n = 0;
+		c = 1;
 	}
 	return 0;
 }
