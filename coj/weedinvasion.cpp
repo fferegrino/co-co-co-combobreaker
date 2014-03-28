61ef22212a9f27cdd7b74aed4bc569276a62ee74
Antonio Feregrino <antonio@fferegrino.org>
2014-03-28 07:13:28 -0600

    Weed Invasion

--- coj/weedinvasion.cpp
+++ coj/weedinvasion.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <cstring>
 #include <queue>
-#define MAX 101
+#define MAX 109
 using namespace std;
 char farm[MAX][MAX];
 bool visita[MAX][MAX];
@@ -18,24 +18,27 @@ int X, Y, wx, wy;
 int weed(){
 	int w = 0;
 	punto p;
-	p.x = wx -1;
-	p.y = Y - wy;
+	p.x = Y - wy;
+	p.y = wx -1;
 	p.d = 0;
-	printf("%d %d\n",p.x, p.y);
 	puntos.push(p);
 	while(!puntos.empty()){
 		punto pto = puntos.front();
 		puntos.pop();
-		if(pto.x < 0 || pto.y < 0 || pto.x >= X || pto.y >= Y || farm[ pto.x][ pto.y] == '*')
-			continue;
 		visita[pto.x][pto.y] = true;
 		for(int i = 0; i < 8; i++){
 			punto pp;
 			pp.x = pto.x + mx[i];
 			pp.y = pto.y + my[i];
 			if(!visita[pp.x][pp.y]){
-				visita[pp.x][pp.y] = true;
 				pp.d = pto.d + 1;
+				if(pp.x < 0 || pp.y < 0 || pp.y >= X || pp.x >= Y){
+					continue;
+				}
+				if(farm[pp.x][pp.y] != '.'){
+					continue;
+				}
+				visita[pp.x][pp.y] = true;
 				w = pp.d >= w ? pp.d : w;
 				puntos.push(pp);
 			}
@@ -48,8 +51,7 @@ int main(){
 	scanf("%d%d%d%d",&X,&Y,&wx,&wy);
 	for(int i = 0; i < Y; i++){
 		scanf("%s",farm[i]);
-		printf("%s\n",farm[i]);
 	}
-	printf("%d",weed()-1);
+	printf("%d",weed());
 	return 0;
 }
