faf2dc1547eb5eecec54b5b9e6d0f75c854800fb
Antonio Feregrino <antonio@fferegrino.org>
2014-04-03 19:18:41 -0600

    Código más bonito y así

--- coj/RoundingProbabilities.cs
+++ coj/RoundingProbabilities.cs
@@ -1,41 +1,34 @@
 using System;
 class RoundingProbabilities{
-	static string N;
-	static void Print(string xD, int l, char a){
+	static void Print(string num, int l, char a){
 		for(int i = 0; i < l-1; i++){
-			Console.Write(xD[i]);
+			Console.Write(num[i]);
 		}
-		char r = (char)((char)xD[l-1] + (char)a);
+		char r = (char)((char)num[l-1] + (char)a);
 		Console.WriteLine(r);
 	}
-	static void Main(string[] args)
+	
+	static void Main(string [] args)
 	{
-		N = Console.ReadLine();
-		string [] st = N.Split(' ');
-		int intpos = 0;
-		for(int i = 1; i < 10; i++){
-			if(!string.IsNullOrEmpty(st[i])){
-			 intpos = int.Parse(st[i]);
-				break;
-			 }
-		}
-		int pp = st[0].IndexOf('.');
-		int nlen= pp+intpos+1;
-		if(intpos == 0)
-			Console.WriteLine(st[0].Substring(0,pp));
+		string N = Console.ReadLine();
+		string [] num = N.Split(' ');
+		int ix = 1;
+		int dec = 0;
+		while(!int.TryParse(num[ix++], out dec));
+		int dot = num[0].IndexOf('.');
+		int nlen= dot+dec+1;
+		if(dec == 0)
+			Console.WriteLine(num[0].Substring(0,dot));
 		else
-			if(nlen < st[0].Length){
-				if((char)(st[0][nlen ]) < '5'){
-					Print(st[0],nlen,(char)0);
-				}
-				else{
-					Print(st[0],nlen,(char)1);
-				}
+			if(nlen < num[0].Length){
+				if((char)(num[0][nlen ]) < '5')
+					Print(num[0],nlen,(char)0);
+				else
+					Print(num[0],nlen,(char)1);
 			}else{
-				Console.Write(st[0]);
-				for(int i = 0; i < nlen - st[0].Length; i++){
+				Console.Write(num[0]);
+				for(int i = 0; i < nlen - num[0].Length; i++)
 					Console.Write("0");
-				}
 				Console.WriteLine("");
 			}
 	}
