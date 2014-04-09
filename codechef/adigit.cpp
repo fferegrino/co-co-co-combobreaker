9324a7c0ed72be6b8620cfe10c5e5a1ac28e95f9
Antonio Feregrino <antonio@fferegrino.org>
2014-04-08 22:51:25 -0500

    Chef and Digits [SOLVED]

--- codechef/adigit.cpp
+++ codechef/adigit.cpp
@@ -1,24 +1,31 @@
 #include <cstdio>
-#include <vector>
-#define MAXS 10005 
+#include <cmath>
+#define MAXS 100005 
 using namespace std;
 char s[MAXS];
+int mem[10][MAXS];
 int n = 0, m = 0, q = 0;
 int calcb(int step){
 	int b2 = 0, b1 = 0;
 	int t = 0;
-	for(int i = 0; i < step; i++){
+	for(int i = step-1; i >= 0; i--){
 		t = (s[step] - '0') - (s[i] - '0');
-		if(t < 0)
-			b2 += t;
-		else
-			b1 += t;
+		if(t == 0){
+			b2 += mem[s[step] - '0'][i];
+			mem[s[step] - '0'][step] = b2;
+			break;
+		}
+		b2 += abs(t);
+		mem[s[step] - '0'][step] = b2;
 	}
-	return b1 - b2;
+	return b2;
 }
 int main(){
 	scanf("%d%d",&n,&m);
 	scanf("%s",s);
+	for(int i = 0; i < n; i++){
+		calcb(i);
+	}
 	while(m--){
 		scanf("%d",&q);
 		printf("%d\n",calcb(q-1));
