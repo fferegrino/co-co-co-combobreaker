567b21d2408265ba69cc8fcdcf072e8563281d06
Antonio Feregrino <antonio@fferegrino.org>
2014-04-27 07:26:34 -0500

    Heads or Tails [WA]

---
+++ codeforces/headsortails.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+#include <utility>
+typedef  std::pair<int, int> pint;
+pint tiros[10001];
+bool operator<(const pint& a, const pint& b){ 
+    return a.first == b.first ? a.second < b.second : a.first < b.first;
+}
+int a, b, x, y;
+int main(){
+	scanf("%d%d%d%d",&x,&y,&a,&b);
+	a = std::max(b+1,a);
+	int t = 0;
+	for(;b <= y; b++){
+		for(int ax = a ;ax <= x; ax++){
+				tiros[t].first = ax;
+				tiros[t++].second = b;
+		}
+		a++;
+	}
+	std::sort(tiros, tiros+t);
+	printf("%d\n", t);
+	for(int i = 0; i < t;i++){
+		//printf("%d %d\n", tiros[i].first, tiros[i].second);
+	}
+	return 0;
+}
